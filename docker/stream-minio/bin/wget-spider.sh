200fc07010bb02fc31434ee471ca3ec01dd159d0
Junmin Ahn <junmin@button.is>
2020-04-28 11:27:25 -0700

    test minio dag image

--- docker/stream-minio/bin/wget-spider.sh
+++ docker/stream-minio/bin/wget-spider.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -rnd --spider --delete-after --force-html --user "$USER" --password "$PASSWORD" -l $DEPTH "$WEBSITE" 2>&1 \
-| awk '/^--/ {u=$3} /^HTTP request sent, awaiting response... / {s=$6} /^Length: .*\[(.+)\]$/ {t=$NF} /^$/ {printf "%s\n",u}' | egrep "$FILTER" | sort | uniq
+wget -rnd --spider --delete-after --force-html --user "$USER" --password "$PASSWORD" -l "2" "https://nodejs.org/dist/" 2>&1 \
+| awk '/^--/ {u=$3} /^HTTP request sent, awaiting response... / {s=$6} /^Length: .*\[(.+)\]$/ {t=$NF} /^$/ {printf "%s\n",u}' | egrep "\.png|\.svg" | sort | uniq
